dbb645acdb0808fa026aa81acb5a1a5c8bea30d7
jonghopark1014 <ds.jongho1014@gmail.com>
2024-04-14 17:39:56 +0900

    update

---
+++ src/main/java/com/jonghopark/kpt/KptApplication.java
@@ -0,0 +1,13 @@
+package com.jonghopark.kpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonghopark/kpt/KptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonghopark.kpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
